e313340d62fcad4503f2af24946f841059e94a49
liwei <liwei@1106185321427486.onaliyun.com>
2025-09-22 19:07:23 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import logo from '../../public/logo.png';
+</script>
+
 <template>
-  <div class="home-container flex-center h-full">
-    <h1>搬砖后台管理系统</h1>
+  <div class="flex-wrap flex-center h-full">
+    <p align="center">
+      <img width="200px" :src="logo" />
+      <p class="mt-10">少写点烂代码，多摸点真·鱼。加油！！！</p>
+    </p>
   </div>
 </template>
