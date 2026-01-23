4b6af11ba691e0105d5e47669e4cde19c6b1bfe5
lw56777 <535847787@qq.com>
2026-01-23 14:37:40 +0800

    fix: useRequest 支持多个请求

--- packages/hooks/useRequest.ts
+++ packages/hooks/useRequest.ts
@@ -2,22 +2,42 @@ import { ref, toRef } from 'vue';
 import { debounce, cloneDeep } from 'lodash-es';
 import { isArray } from '@element-plus-lego/utils';
 
-export type TService = (...args: any[]) => Promise<any> | Promise<any>[];
+export type TServiceFunction = (...args: any[]) => Promise<any>;
 
-export interface IOptions {
+export interface IOptionsBase {
   initData?: any; // 初始值
   immediate?: boolean; // 是否立即执行
   delay?: number; // 防抖等待时间
-  params?: ReturnType<typeof toRef>; // 请求参数
 }
 
-const defaultOptions: IOptions = {
+export interface IOptionsSingle extends IOptionsBase {
+  params?: any; // 单个函数时，参数可以是任意值
+}
+
+export interface IOptionsArray extends IOptionsBase {
+  params?: any[]; // 函数数组时，参数必须是数组
+}
+
+const defaultOptions = {
   immediate: true,
   delay: 0,
   params: {},
 };
 
-export function useRequest(service: TService, options?: IOptions) {
+// 函数重载：当 service 是数组时，params 必须是数组
+export function useRequest(
+  service: TServiceFunction[],
+  options?: IOptionsArray,
+): any;
+// 函数重载：当 service 是单个函数时，params 可以是任意值
+export function useRequest(
+  service: TServiceFunction,
+  options?: IOptionsSingle,
+): any;
+export function useRequest(
+  service: TServiceFunction | TServiceFunction[],
+  options?: IOptionsSingle | IOptionsArray,
+): any {
   const {
     initData,
     immediate,
@@ -39,8 +59,8 @@ export function useRequest(service: TService, options?: IOptions) {
     const queue: Promise<any>[] = [];
 
     if (isArr) {
-      service.forEach(item => {
-        queue.push(item(params.value));
+      service.forEach((item, index) => {
+        queue.push(item(params.value[index]));
       });
     } else {
       queue.push(service(params.value) as Promise<any>);
@@ -48,7 +68,9 @@ export function useRequest(service: TService, options?: IOptions) {
 
     Promise.all(queue)
       .then(res => {
-        data.value = isArr ? res.map(item => item.data) : res[0].data;
+        data.value = isArr
+          ? res.map(item => item?.data || item)
+          : res[0]?.data || res[0];
       })
       .catch(err => {
         error.value = err;
